b76dbf8a9ef4efb330400e9a78e81612fe778497
liamkande <liamkande@Ansoumanes-MBP.home>
2017-08-24 09:59:26 -0400

    development and production ENV

---
+++ builds/production/js/flexslider.js
@@ -0,0 +1,2 @@
+
+$(window).load(function(){$('.front-slider').flexslider({animation:"slide",controlNav:false,directionNav:false});$('.story-slider').flexslider({animation:"slide",controlNav:true,directionNav:true});});
